991d1cc956f7a30340c716afe8f5c2c023e343fd
Theo Sun <theo.sun@sap.com>
2021-10-12 12:47:18 +0800

    feat: mysql example

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# odata-v4-cap-demo
+# CAP Demo for `cds-mysql`
+
+## Setup
+
+1. create a `.env` file for development, fill the database connection information
+2. execute `npm run deploy` to deploy the database schema to database
+3. execute `npm start` to start the server
+4. execute `npm run test` to perform unit test
+
+## Environment
+
+* CDS_MYSQL_USER
+* CDS_MYSQL_PASSWORD
+* CDS_MYSQL_DATABASE
+* CDS_MYSQL_HOST
+* CDS_MYSQL_PORT
+
+
+`.env` file example
+
+```
+CDS_MYSQL_USER=cdstest
+CDS_MYSQL_PASSWORD=cdstest
+CDS_MYSQL_DATABASE=cdstest
+CDS_MYSQL_HOST=mysql
+CDS_MYSQL_PORT=3306
+```
---
+++ test/example.test.js
@@ -0,0 +1,33 @@
+const cds = require('@sap/cds/lib');
+const cdstest = cds.test(__dirname + '/..')
+
+
+describe('Example Test Case', () => {
+
+  it('should support query', async () => {
+
+    const response = await cdstest.get('/odata/Peoples')
+    expect(response.status).toBe(200)
+    expect(response.data.value).toHaveLength(1)
+
+  });
+
+  it('should support create and delete', async () => {
+
+    // create a instance
+    const response = await cdstest.post('/odata/Peoples', {
+      UserName: 'theo2'
+    })
+
+    expect(response.status).toBe(201)
+    expect(response.data.UserName).toBe('theo2')
+
+    // delete created item
+    const response2 = await cdstest.delete(`/odata/Peoples(${response.data.ID})`)
+    expect(response2.status).toBe(204)
+
+
+  });
+
+
+});
